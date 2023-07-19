44e2e0167276258ec68e0051498a9855805dfc87
YassineElbouchaibi <YassineElbouchaibi@users.noreply.github.com>
2023-07-19 15:06:31 +0000

    deployed via Gatsby Publish Action 🎩 for 45bc5e8ad91e3454ad550ad4a562864bf950d681

---
+++ component---src-pages-404-tsx-e02f2e79edfd8ca44e9a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_tailwind_mdx_blog=self.webpackChunkgatsby_starter_tailwind_mdx_blog||[]).push([[218],{6040:function(t,e,a){a.r(e);var n=a(7294),l=a(5441),s=a(1785);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:i},n.createElement(s.pQ,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-e02f2e79edfd8ca44e9a.js.map
